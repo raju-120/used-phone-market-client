1aec7875ba82a2e8c1ed8f71cacc1ecde5ec6389
raju120 <ashfaqraju27@gmail.com>
2024-11-07 11:48:29 +0600

    title name changed

--- src/UseHooks/UseTitle/UseTitle.js
+++ src/UseHooks/UseTitle/UseTitle.js
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
 
-
-const useTitle =(title)=>{
-    useEffect(()=>{
-        document.title= `${title}-Resell Market`
-    },[title]);
-}
+const useTitle = (title) => {
+  useEffect(() => {
+    document.title = `${title}-Resale Market`;
+  }, [title]);
+};
 
 export default useTitle;
